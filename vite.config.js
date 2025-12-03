d5f7c82ba5a5ae34384ec2b524e6e8d7e8340737
Ahnaf Akif <akif289789@gmail.com>
2025-12-03 20:41:46 +0600

    cdi

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                     // ← add this line
-  build: {
-    outDir: 'dist',
-    sourcemap: true
+  define: {
+    __firebase_config: JSON.stringify({
+      apiKey: "AIzaSyCEnCc-nI4FVLHxkb__nzXTWSGJ0xzTTtM",
+      authDomain: "studyhelper-1.firebaseapp.com",
+      projectId: "studyhelper-1",
+      storageBucket: "studyhelper-1.firebasestorage.app",
+      messagingSenderId: "137163148790",
+      appId: "1:137163148790:web:d3ef16e955a3ad854acbd7"
+    }),
+    __app_id: JSON.stringify("devstudy-local"),
+    __initial_auth_token: JSON.stringify("")
   }
 });
